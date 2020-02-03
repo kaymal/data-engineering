88403153a450ec15a64c4b57c6f8ef6b0dd6a01c
kaymal <gutkyle@gmail.com>
2020-02-03 21:32:09 +0100

    Create files for software-engineering.

---
+++ data/se/bad_pep8.py
@@ -0,0 +1,8 @@
+# Example non-compliant code
+
+x=2  # E225 missing whitespace around operator
+
+if x < 5:
+ x += 1  # E111 indentation is not a multiple of four
+
+print( x  )  # 201 whitespace after '(',  E202 whitespace before ')'
---
+++ data/se/good_pep8.py
@@ -0,0 +1,8 @@
+# Example PEP 8 compliant code
+
+x = 2
+
+if x < 5:
+    x += 1
+
+print(x)
